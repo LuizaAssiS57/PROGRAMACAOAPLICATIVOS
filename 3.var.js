2be00942dc083c1cc670ca6c7987938b5c5e15dd
LuizaAssiS57 <luizaassis57@gmail.com>
2026-03-16 13:51:40 -0300

    base do projeto.

--- 3.var.js
+++
@@ -1,6 +0,0 @@
-// Variáveis.
-
-// Declaração de variáveis em projetos antigos/legado.
-var nome = "Marta"
-
-console.log("Nome: ", nome)
--- 7.laco-for.js
+++
@@ -1,5 +0,0 @@
-// Laço de repetição: for
-
-for (let i = 1; i <= 5; i++) {
-    console.log(i)
-}
